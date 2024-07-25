1dc6427b84ae31ecf6e7bbfbe858cc7d4b450d1d
justaguy6 <outerjose@gmail.com>
2024-07-25 18:23:35 -0300

    Android Source

---
+++ source/MusicBeatState.hx
@@ -0,0 +1,270 @@
+package;
+
+import flixel.FlxSubState;
+import flixel.FlxCamera;
+import flixel.util.FlxColor;
+import openfl.Lib;
+import Conductor.BPMChangeEvent;
+import flixel.FlxG;
+import flixel.addons.ui.FlxUIState;
+
+import flixel.addons.transition.Transition;
+#if android
+import android.AndroidControls;
+import android.flixel.FlxVirtualPad;
+import flixel.input.actions.FlxActionInput;
+import flixel.util.FlxDestroyUtil;
+#end
+class MusicBeatState extends FlxUIState
+{
+	private var lastBeat:Float = 0;
+	private var lastStep:Float = 0;
+
+	private var curStep:Int = 0;
+	private var curBeat:Int = 0;
+	private var curDecimalBeat:Float = 0;
+	private var controls(get, never):Controls;
+
+	public var camTransition:FlxCamera;
+
+	inline function get_controls():Controls
+		return PlayerSettings.player1.controls;
+
+	#if android
+	var virtualPad:FlxVirtualPad;
+	var androidControls:AndroidControls;
+	var trackedinputsUI:Array<FlxActionInput> = [];
+	var trackedinputsNOTES:Array<FlxActionInput> = [];
+
+	public function addVirtualPad(DPad:FlxDPadMode, Action:FlxActionMode)
+	{
+		virtualPad = new FlxVirtualPad(DPad, Action);
+		add(virtualPad);
+
+		controls.setVirtualPadUI(virtualPad, DPad, Action);
+		trackedinputsUI = controls.trackedinputsUI;
+		controls.trackedinputsUI = [];
+	}
+
+	public function removeVirtualPad()
+	{
+		if (trackedinputsUI != [])
+			controls.removeFlxInput(trackedinputsUI);
+
+		if (virtualPad != null)
+			remove(virtualPad);
+	}
+
+	public function addAndroidControls()
+	{
+		androidControls = new AndroidControls();
+    androidControls.alpha = 0.8;
+    
+		switch (AndroidControls.getMode())
+		{
+			case 0 | 1 | 2: // RIGHT_FULL | LEFT_FULL | CUSTOM
+				controls.setVirtualPadNOTES(androidControls.virtualPad, RIGHT_FULL, NONE);
+			case 3: // BOTH_FULL
+				controls.setVirtualPadNOTES(androidControls.virtualPad, BOTH_FULL, NONE);
+			case 4: // HITBOX
+				controls.setHitBox(androidControls.hitbox);
+			case 5: // KEYBOARD
+		}
+
+		trackedinputsNOTES = controls.trackedinputsNOTES;
+		controls.trackedinputsNOTES = [];
+
+		var camControls = new flixel.FlxCamera();
+		FlxG.cameras.add(camControls, false);
+		camControls.bgColor.alpha = 0;
+
+		androidControls.cameras = [camControls];
+		androidControls.visible = false;
+		add(androidControls);
+	}
+
+	public function removeAndroidControls()
+	{
+		if (trackedinputsNOTES != [])
+			controls.removeFlxInput(trackedinputsNOTES);
+
+		if (androidControls != null)
+			remove(androidControls);
+	}
+
+	public function addPadCamera()
+	{
+		if (virtualPad != null)
+		{
+			var camControls = new flixel.FlxCamera();
+			FlxG.cameras.add(camControls, false);
+			camControls.bgColor.alpha = 0;
+			virtualPad.cameras = [camControls];
+		}
+	}
+	#end
+
+	override function destroy()
+	{
+		#if android
+		if (trackedinputsNOTES != [])
+			controls.removeFlxInput(trackedinputsNOTES);
+
+		if (trackedinputsUI != [])
+			controls.removeFlxInput(trackedinputsUI);
+		#end
+
+		super.destroy();
+
+		#if android
+		if (virtualPad != null)
+		{
+			virtualPad = FlxDestroyUtil.destroy(virtualPad);
+			virtualPad = null;
+		}
+
+		if (androidControls != null)
+		{
+			androidControls = FlxDestroyUtil.destroy(androidControls);
+			androidControls = null;
+		}
+		#end
+	}
+	override function create()
+	{
+		TimingStruct.clearTimings();
+		Main.setFPSCap(FlxG.save.data.framerate);
+
+		super.create();
+	}
+
+	static var array:Array<FlxColor> = [
+		FlxColor.fromRGB(148, 0, 211),
+		FlxColor.fromRGB(75, 0, 130),
+		FlxColor.fromRGB(0, 0, 255),
+		FlxColor.fromRGB(0, 255, 0),
+		FlxColor.fromRGB(255, 255, 0),
+		FlxColor.fromRGB(255, 127, 0),
+		FlxColor.fromRGB(255, 0 , 0)
+	];
+
+	var skippedFrames = 0;
+
+	override function update(elapsed:Float)
+	{
+		var nextStep:Int = updateCurStep();
+
+		if (nextStep >= 0)
+		{
+			if (nextStep > curStep)
+			{
+				for (i in curStep...nextStep)
+				{
+					curStep++;
+					updateBeat();
+					stepHit();
+				}
+			}
+			else if (nextStep < curStep)
+			{
+				//Song reset?
+				curStep = nextStep;
+				updateBeat();
+				stepHit();
+			}
+		}
+
+		if (Conductor.songPosition < 0)
+			curDecimalBeat = 0;
+		else
+		{
+			if (TimingStruct.AllTimings.length > 1)
+			{
+				var data = TimingStruct.getTimingAtTimestamp(Conductor.songPosition);
+
+				Conductor.crochet = ((60 / data.bpm) * 1000);
+
+				curDecimalBeat = data.startBeat + (((Conductor.songPosition/1000) - data.startTime) * (data.bpm / 60));
+			}
+			else
+			{
+				curDecimalBeat = (Conductor.songPosition / 1000) * (Conductor.bpm/60);
+				Conductor.crochet = ((60 / Conductor.bpm) * 1000);
+			}
+		}
+
+		if (FlxG.save.data.fpsRain && skippedFrames >= 6)
+		{
+			if (currentColor >= array.length)
+				currentColor = 0;
+			(cast (Lib.current.getChildAt(0), Main)).changeFPSColor(array[currentColor]);
+			currentColor++;
+			skippedFrames = 0;
+		}
+		else
+			skippedFrames++;
+
+		var fps:Int = FlxG.save.data.framerate;
+		if (Main.getFPSCap() != fps && fps <= 290)
+			Main.setFPSCap(fps);
+
+		#if debug
+		if(FlxG.keys.justPressed.F3) {
+			@:privateAccess
+			for (key in FlxG.bitmap._cache.keys()) {
+				var bitmap = FlxG.bitmap._cache.get(key);
+				if(bitmap != null) {
+					trace('"' + key + '" uses ${bitmap.width * bitmap.height * 4} (${bitmap.width}x${bitmap.height})');
+				}
+			}
+		}
+		#end
+
+		super.update(elapsed);
+	}
+
+	override function openSubState(subState:FlxSubState) {
+		super.openSubState(subState);
+
+		if((subState is Transition)) {
+			if(camTransition != null) {
+				subState.cameras = [camTransition];
+			}
+		}
+	}
+
+	private function updateBeat():Void
+	{
+		lastBeat = curBeat;
+		curBeat = Math.floor(curStep / 4);
+	}
+
+	public static var currentColor = 0;
+
+	private function updateCurStep():Int
+	{
+		var lastChange:BPMChangeEvent = {
+			stepTime: 0,
+			songTime: 0,
+			bpm: 0
+		}
+		for (i in 0...Conductor.bpmChangeMap.length)
+		{
+			if (Conductor.songPosition >= Conductor.bpmChangeMap[i].songTime)
+				lastChange = Conductor.bpmChangeMap[i];
+		}
+
+		return lastChange.stepTime + Math.floor((Conductor.songPosition - lastChange.songTime) / Conductor.stepCrochet);
+	}
+
+	public function stepHit():Void
+	{
+		if (curStep % 4 == 0)
+			beatHit();
+	}
+
+	public function beatHit():Void
+	{
+		//do literally nothing dumbass
+	}
+}
